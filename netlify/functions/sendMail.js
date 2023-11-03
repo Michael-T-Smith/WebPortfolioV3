278d9de5f2b8ba7ce78ab843a6d82c4ba521fbce
Michael Smith <88092513+MichaelT-Smith@users.noreply.github.com>
2023-11-03 13:09:04 -0400

    removing mail until new service is found

--- netlify/functions/sendMail.js
+++ netlify/functions/sendMail.js
@@ -1,10 +1,3 @@
-const nodemailer = require('nodemailer');
-
-exports.handler = async function (event, context) {
-    const { fullname, email, message } = JSON.parse(event.body);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Email sent successfully' }),
-    };
-};
+export function sendMail(message) {
+  console.log(message);
+}
